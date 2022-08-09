6acfa0962a9e60d66a8082a0654cb02b95b20569
Bima Renthadinata <bimarnt29@gmail.com>
2022-08-09 10:30:29 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,20 +5,20 @@ const fs = require('fs').promises;
 const path = require('path');
 
 const {Storage} = require('@google-cloud/storage');
-const { resolve } = require('path');
-const { info } = require('console');
+const {resolve} = require('path');
+const {info} = require('console');
 const storage = new Storage();
 
 const {DESTINATION_BUCKET_NAME} = process.env;
 
 exports.imageprocessing = async event => {
-    const object = event;
+  const object = event;
 
-    const file = storage.bucket(object.bucket).file(object.name);
-    const filePath = `gs://${object.bucket}/${object.name}`;
+  const file = storage.bucket(object.bucket).file(object.name);
+  const filePath = `gs://${object.bucket}/image/${object.name}`;
 
-    console.log(`Getting ${file.name}.`);
-  };
+  console.log(`Getting ${file.name}.`);
+};
 
 const imageprocess = async (file, destinationBucketName) => {
 
@@ -27,13 +27,12 @@ const imageprocess = async (file, destinationBucketName) => {
     
   // Download file from bucket.
   try {
-      await file.download({destination: tempLocalPath});
-      console.log(`Downloaded ${file.name} to ${tempLocalPath}.`);
-      }
-
-  catch (err) {
+    await file.download({destination: tempLocalPath});
+      
+    console.log(`Downloaded ${file.name} to ${tempLocalPath}.`);
+  } catch (err) {
     throw new Error(`File download failed: ${err}`);
-    }
+  }
 
   await new Promise((resolve, reject) => {
     sharp(tempLocalPath)
@@ -41,7 +40,7 @@ const imageprocess = async (file, destinationBucketName) => {
         width: 200,
         height: 200,
         })
-      .toFile(thumbnail+'_thumb'+'.jpg', (err, info) => {
+      .toFile(tempLocalPath, (err, info) => {
         if (err) {
           console.error('Failed to process image.', err);
           reject(err);
@@ -51,7 +50,7 @@ const imageprocess = async (file, destinationBucketName) => {
         }
       });
 
-  });
+});
     
 /*   
   const webp = sharp(tempLocalPath)
@@ -64,13 +63,13 @@ const imageprocess = async (file, destinationBucketName) => {
   const destinationBucket = storage.bucket(destinationBucketName);
 
   const gcsPath = `gs://${destinationBucketName}/${file.name}`;
-    try {
-      await destinationBucket.upload(thumbnail, {destination: file.name});
-      console.log(`Uploaded processed image to: ${gcsPath}`);
-    } catch (err) {
-      throw new Error(`Unable to upload processed image to ${gcsPath}: ${err}`);
-    }
+  try {
+    await destinationBucket.upload(tempLocalPath, {destination: file.name});
+    console.log(`Uploaded processed image to: ${gcsPath}`);
+  } catch (err) {
+    throw new Error(`Unable to upload processed image to ${gcsPath}: ${err}`);
+  }
 
     // Delete the temporary file.
     return fs.unlink(tempLocalPath);
-}
+};
